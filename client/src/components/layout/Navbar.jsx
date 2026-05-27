3117b9d505bbaaaf8a1fb44383d384221b197544
Tabitha Sulaiman <tabisul04@gmail.com>
2026-05-26 21:03:28 -0700

    v2.1: Added my logo and updated title of browser.

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -1,21 +1,23 @@
 import { NavLink } from "react-router-dom";
+import tsLogo from "../../assets/brand/ts-logo.png";
 
 function Navbar() {
-  return (
-    <header className="site-header">
-      <NavLink className="site-logo" to="/">
-        Tabitha Sulaiman
-      </NavLink>
+    return (
+        <header className="site-header">
+            <NavLink className="site-logo" to="/" aria-label="Tabitha Sulaiman home">
+                <img className="site-logo-mark" src={tsLogo} alt="" />
+                <span className="site-logo-text">Tabitha Sulaiman</span>
+            </NavLink>
 
-      <nav className="site-nav" aria-label="Main navigation">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/projects">Projects</NavLink>
-        <NavLink to="/experience">Experience</NavLink>
-        <NavLink to="/systems-thinking">Systems</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
-      </nav>
-    </header>
-  );
+            <nav className="site-nav" aria-label="Main navigation">
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/projects">Projects</NavLink>
+                <NavLink to="/experience">Experience</NavLink>
+                <NavLink to="/systems-thinking">Systems</NavLink>
+                <NavLink to="/contact">Contact</NavLink>
+            </nav>
+        </header>
+    );
 }
 
 export default Navbar;
